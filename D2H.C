387c8f0e133d9bb9b43f2d62452a1f9ab47f88f0
SamHakke <sameerhakke10@gmail.com>
2020-05-16 16:26:30 +0530

    Decimal to Hexadecimal conversion

---
+++ D2H.C
@@ -0,0 +1,29 @@
+//Convert the decimal number to hexadecimal.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    int num=26,i,temp;
+     char hex[10];
+ clrscr();
+       for(i=0;num>0;i++)
+       { //temp=0;
+	 temp=num%16;
+		if(temp<10)
+		{
+		     hex[i]=temp+48;
+		}
+		else
+		{
+		     hex[i]=temp+55;
+		}
+
+	  num=num/16;
+       }
+
+       printf("\n Octal Equivalent is : ");
+       for(i=i-1;i>=0;i--)
+       {
+		printf("%c",hex[i]);
+       }
+ getch();
+}
